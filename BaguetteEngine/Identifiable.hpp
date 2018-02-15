e047ecd367c3842dd0bf6209d4002387913c3b42
galibe_s <stephane.galibert@epitech.eu>
2018-02-15 00:05:20 -0500

    Base

--- BaguetteEngine/Identifiable.hpp
+++ BaguetteEngine/Identifiable.hpp
@@ -1,1 +1,7 @@
 #pragma once
+
+struct Identifiable
+{
+	virtual ~Identifiable(void) {}
+	virtual int getID(void) const = 0;
+};
--- BaguetteEngine/NonCopyable.hpp
+++ BaguetteEngine/NonCopyable.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+struct NonCopyable
+{
+	NonCopyable(void) {}
+	~NonCopyable(void) {}
+	NonCopyable(const NonCopyable & other) = delete;
+	NonCopyable & operator=(const NonCopyable & other) = delete;
+};
