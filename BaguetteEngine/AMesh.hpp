29bf356b08bf4c671fb4a4e8ba4eb30ccb1564e8
Anthony Vogelweid <anthony.vogelweid.1@ulaval.ca>
2018-03-09 19:46:10 -0500

    Gestion des modèles 3D en cours

--- BaguetteEngine/AMesh.hpp
+++ BaguetteEngine/AMesh.hpp
@@ -14,6 +14,7 @@ public:
 	enum InstantiableMesh
 	{
 		UNDEFINED,
+		MODEL,
 		SPHERE,
 		PLANE,
 		CUBE,
--- BaguetteEngine/Model3D.hpp
+++ BaguetteEngine/Model3D.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "ofxAssimpModelLoader.h"
+#include "AMesh.hpp"
+
+class Model3D : public AMesh
+{
+public:
+	Model3D();
+	virtual ~Model3D();
+	bool load(const string &path);
+
+private:
+	ofxAssimpModelLoader		model_;
+};
