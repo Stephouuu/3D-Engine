118089f6866d3df03c95a50cbccdc99c59996686
stephouuu <stephane.galibert@epitech.eu>
2018-03-11 03:05:48 -0400

    forme vectorielle

--- BaguetteEngine/MainMenu.hpp
+++ BaguetteEngine/MainMenu.hpp
@@ -21,8 +21,6 @@ public:
 	virtual void refresh(int newEditorDimension);
 	virtual void focus(const Identifiable & id);
 	virtual void windowsResized(const ofPoint & size);
-	void setFromXYValues(ofVec2f & vec);
-	void setFromWHValues(ofVec2f & vec);
 
 	bool wantScreenshot(void);
 
@@ -62,6 +60,7 @@ private:
 	ofxButton			insertRectangle_;
 
 	ofxButton			insertDialogVector_;
+	ofxButton			insertSmileVector_;
 
 	ofxButton			swapMode_;
 
